3ce62d6c58aef108727781be5125829a5893aace
renanbrognolitke <renan.brognoli.external@tkelevator.com>
2026-07-09 18:13:05 -0300

    Publish Studio 1.0.6 download metadata

--- src/lib/downloads.ts
+++ src/lib/downloads.ts
@@ -6,12 +6,12 @@ export type StudioDownloadInfo = {
 };
 
 export const DEFAULT_WINDOWS_DOWNLOAD_URL =
-  "https://github.com/renanbrognoli01/Dax_Descriptions/releases/download/v1.0.5/BrognoliStudio-Setup-1.0.5.exe";
-export const DEFAULT_WINDOWS_VERSION = "1.0.5";
+  "https://github.com/renanbrognoli01/Dax_Descriptions/releases/download/v1.0.6/BrognoliStudio-Setup-1.0.6.exe";
+export const DEFAULT_WINDOWS_VERSION = "1.0.6";
 export const DEFAULT_WINDOWS_MIN_OS = "Windows 10 or later";
-export const DEFAULT_WINDOWS_SHA256 = null;
+export const DEFAULT_WINDOWS_SHA256 = "0A038FFADEF2DACD82A0BB1C916ED43F3A6F035782612570FA6D8E8A57E951F8";
 export const DEFAULT_RELEASE_PAGE_URL =
-  "https://github.com/renanbrognoli01/Dax_Descriptions/releases/tag/v1.0.5";
+  "https://github.com/renanbrognoli01/Dax_Descriptions/releases/tag/v1.0.6";
 
 export function getStudioDownloadInfo(): StudioDownloadInfo {
   const windowsUrl =
